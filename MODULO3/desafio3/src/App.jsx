fa846365eb4ad0f398f17f3aec004bc661a69249
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2026-06-23 18:55:24 -0300

    criando novo modulo3. Add projeto mexendo com img

---
+++ MODULO3/desafio3/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import Paisagem2 from './assets/img2.jpg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <h1>Seção 3</h1>
+      </header>
+
+      <section>
+        {/* Imagem em Public */}
+        <img src="/img1.jpg" alt="Paisagem1" className='imgPaisagem'/>
+      </section>
+
+      <section>
+        {/* Imagem em Assets */}
+        <img src={Paisagem2} alt="Paisagem2" className='imgPaisagem'/>
+      </section>
+    </>
+  )
+}
+
+export default App
