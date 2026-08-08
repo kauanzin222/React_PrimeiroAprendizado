0a5062ba337eb1a584cdd1f900c27bded200e1f5
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2026-08-08 20:28:50 -0300

    aprendendo sobre forms react

---
+++ Formularios/form-project/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import MyForm from './components/MyForm'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header className='container'>
+        <h1>Módulo de Forms</h1>
+      </header>
+
+      <MyForm></MyForm>
+    </>
+  )
+}
+
+export default App
---
+++ Formularios/form-project/src/components/MyForm.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import styles from './MyForm.module.css'
+
+const MyForm = () => {
+  // 3- Gerenciamento de Dados
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('Enviando o form')
+    console.log(name, email)
+  }
+
+  return (
+    <div className='container'>
+      {/* 5- Envio do Form */}
+      <form onSubmit={handleSubmit}>
+        <h2>Formulário Inicial</h2>
+        <hr />
+        <div className={styles.sectionForm}>
+          <label htmlFor="name">Nome: </label>
+          <input type="text" name='name' placeholder='Digite o seu nome' className={styles.inputForm} onChange={handleName} />
+        </div>
+
+        {/* 2- Label envolvendo Input */}
+        <div className={styles.sectionForm}>
+          <label>
+            <span>E-mail</span>
+            {/* 4- Simplificação de Manipulação de State */}
+            <input type="email" name='email' placeholder='Digite o seu e-mail' className={styles.inputForm} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+        </div>
+
+
+        <input type="submit" value='Enviar' className={styles.btnEnviar} />
+      </form>
+    </div>
+  )
+}
+
+export default MyForm
