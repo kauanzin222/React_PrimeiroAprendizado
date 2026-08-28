a130405f08bee51d9656c811e9f05a60c03f073f
kauanzin222 <kauaanthony.cardoso44@gmail.com>
2026-08-28 10:34:56 -0300

    Usando navlink e isActive

--- React-Router/router/src/components/NavBar.tsx
+++ React-Router/router/src/components/NavBar.tsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
     <nav>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/about'}>Sobre</Link>
+        {/* <Link to={'/'}>Home</Link>
+        <Link to={'/about'}>Sobre</Link> */}
+        <NavLink to={'/'}>Home</NavLink>
+        <NavLink to={'/about'}>Sobre</NavLink>
     </nav>
   )
 }
